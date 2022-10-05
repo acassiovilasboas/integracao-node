37049a27d762db4db94b27e7b52a6a33ad8a6359
Acassio Vilas Boas <acassio.dev@outlook.com>
2022-10-04 21:17:21 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// importando os serviços da lib express
+const express = require('express')
+
+// atribuindo a constante server as funcionalidades
+// da função express()
+const server = express()
+server.use(express.json())
+
+// nosso array de comensais
+const deathEaters = [
+    "Aleto Carrow",
+    "Antônio Dolohov",
+    "Avery Jr",
+    "Belatriz Lestrange", 
+    "Draco Malfoy", 
+    "Igor Karkaroff", 
+    "Pedro Pettigrew"
+]
+
+// criando a rota
+server.get('/api', (req, res) => {
+    return res.json( { message: "resposta da minha api" } )
+})
+
+// outra rota
+server.get('/comensais', (req, res) => {
+    return res.json(deathEaters)
+})
+
+// habilitando a porta 3000 para responder como
+// servidor da aplicação
+server.listen(3000)
